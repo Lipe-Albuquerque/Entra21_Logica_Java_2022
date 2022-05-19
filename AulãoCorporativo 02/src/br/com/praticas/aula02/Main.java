60df72c14463d84a52b24d5c16c4fd626a85cfe0
Lipe-Albuquerque <filipe.intel@gmail.com>
2022-05-18 16:22:14 -0900

    aual 13
    laços de repetição com vetores

--- AulãoCorporativo 02/src/br/com/praticas/aula02/Main.java
+++ AulãoCorporativo 02/src/br/com/praticas/aula02/Main.java
@@ -48,29 +48,44 @@ public class Main {
 	private static void converttemperature() {
 		Scanner input = new Scanner(System.in);
 
-		byte tipo;
+		byte tipo[] = { 0, 1 };
 		float temp;
-		linha();
-		System.out.println("Selecione o tipo de Temperatura que deseja converter: ");
-		linha();
-		System.out.println("1- Celsius para Fahrenheit.");
-		System.out.println("	    ou		     	");
-		System.out.println("2- Fahrenheit para Celsius.");
-		linha();
-		tipo = input.nextByte();
 
-		if (tipo == 1) {
-			System.out.println("Digite a temperatura em graus Celsius: ");
-			temp = input.nextFloat();
-			temp = (temp * 9 / 5) + 32;
-			System.out.println("Sua temperatura é: " + temp + " Fahrenheit");
+		do {
+			linha();
+			System.out.println("Selecione o tipo de Temperatura que deseja converter: ");
+			linha();
+			System.out.println("1- Celsius para Fahrenheit.");
+			System.out.println("	    ou		     	");
+			System.out.println("2- Fahrenheit para Celsius.");
+			linha();
+			tipo[0] = input.nextByte();
 
-		} else if (tipo == 2) {
-			System.out.println("Digite a temperatura em graus Fahrenheit: ");
-			temp = input.nextFloat();
-			temp = (temp - 32) * 5 / 9;
-			System.out.println("Sua temperatura é: " + temp + " Celsius");
-		}
+			if (tipo[0] == 1) {
+				System.out.println("Digite a temperatura em graus Celsius: ");
+				temp = input.nextFloat();
+				temp = (temp * 9 / 5) + 32;
+				System.out.println("Sua temperatura é: " + temp + " Fahrenheit");
+				linha();
+				System.out.println("Dseja continuar ?");
+				tipo[1] = input.nextByte();
+
+			} else if (tipo[0] == 2) {
+				System.out.println("Digite a temperatura em graus Fahrenheit: ");
+				temp = input.nextFloat();
+				temp = (temp - 32) * 5 / 9;
+				System.out.println("Sua temperatura é: " + temp + " Celsius");
+				linha();
+				System.out.println("Dseja continuar ? \n 1- Sim \n 2- Não");
+				tipo[1] = input.nextByte();
+				while (tipo[1] != 1 || tipo[1] != 2) {
+					System.out.println("Digite uma opção valida");
+				}
+			} else {
+				System.out.println("digite uma opção valida ");
+			}
+
+		} while (tipo[1] != 2);
 
 	}
 
